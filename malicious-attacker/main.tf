da8a47f58ba492a063e103e7d2f9dc544d56dafb
aneel-sigsci <45185884+aneel-sigsci@users.noreply.github.com>
2022-08-03 11:15:31 -0700

    Adding malicious-attacker example
    This Terraform example allows you to create a request rule to block attack requests from known Malicious IPs

---
+++ malicious-attacker/main.tf
@@ -0,0 +1,96 @@
+terraform {
+  required_providers {
+    sigsci = {
+      source = "signalsciences/sigsci"
+    }
+  }
+}
+
+provider "sigsci" {
+    corp = "[corp-shortname]"       // Required. may also provide via env variable SIGSCI_CORP
+  //  email = ""      // Required. may also provide via env variable SIGSCI_EMAIL
+  //  auth_token = "" //may also provide via env variable SIGSCI_TOKEN
+}
+
+resource "sigsci_site_rule" "test" {
+  site_short_name = "test"
+  type            = "request"
+  group_operator  = "any"
+  enabled         = true
+  reason          = "Blocking attacks from known Malicious IPs"
+  expiration      = ""
+
+
+  conditions {
+    type     = "multival"
+    field    = "signal"
+    group_operator = "any"
+    operator = "exists"
+    conditions {
+      field = "signalType"
+      operator = "equals"
+      type = "single"
+      value = "USERAGENT"
+    }
+    conditions {
+      field = "signalType"
+      operator = "equals"
+      type = "single"
+      value = "BACKDOOR"
+    }
+    conditions {
+      field = "signalType"
+      operator = "equals"
+      type = "single"
+      value = "CMDEXE"
+    }
+    conditions {
+      field = "signalType"
+      operator = "equals"
+      type = "single"
+      value = "SQLI"
+    }
+    conditions {
+      field = "signalType"
+      operator = "equals"
+      type = "single"
+      value = "TRAVERSAL"
+    }
+    conditions {
+      field = "signalType"
+      operator = "equals"
+      type = "single"
+      value = "XSS"
+    }
+  }
+
+    conditions {
+    type     = "multival"
+    field    = "signal"
+    group_operator = "any"
+    operator = "exists"
+    conditions {
+      field = "signalType"
+      operator = "equals"
+      type = "single"
+      value = "SIGSCI-IP"
+    }
+    conditions {
+      field = "signalType"
+      operator = "equals"
+      type = "single"
+      value = "TORNODE"
+    }
+    conditions {
+      field = "signalType"
+      operator = "equals"
+      type = "single"
+      value = "SANS"
+    }
+  }
+
+  actions {
+    type = "block"
+    signal = "corp.malicious-attacker" 
+  }
+}
