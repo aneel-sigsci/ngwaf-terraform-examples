a26b412d73a1d530ae96b31e21ec7518e861b81b
aneel-sigsci <45185884+aneel-sigsci@users.noreply.github.com>
2022-08-16 11:09:39 -0700

    Add files via upload

---
+++ ofac-list-rule/main.tf
@@ -0,0 +1,61 @@
+
+terraform {
+  required_providers {
+    sigsci = {
+      source = "signalsciences/sigsci"
+    }
+  }
+}
+
+provider "sigsci" {
+  //  corp = ""       // Required. may also provide via env variable SIGSCI_CORP
+  //  email = ""      // Required. may also provide via env variable SIGSCI_EMAIL
+  //  auth_token = "" //may also provide via env variable SIGSCI_TOKEN
+}
+
+resource "sigsci_corp_signal_tag" "ofac" {
+  short_name  = "ofac"
+  description = "Countries on OFAC list"
+}
+
+resource "sigsci_corp_list" "ofac" {
+    name = "OFAC Countries"
+    type = "country"
+    entries = [
+        "IR",
+        "SY",
+        "SD",
+        "KP",
+        "UA",
+        "BY",
+        "CI",
+        "CU",
+        "CD",
+        "IQ",
+        "LR",
+        "MM",
+        "ZW",
+    ]
+}
+
+resource "sigsci_corp_rule" "ofac" {
+  site_short_names = []
+  type = "request"
+  corp_scope = "global"
+  enabled = true
+  group_operator = "all"
+  reason = "OFAC Country Blocking Rule"
+  expiration = ""
+
+  conditions {
+    type     = "single"
+    field    = "country"
+    operator = "inList"
+    value = "corp.ofac-countries"
+  }
+
+  actions {
+    type = "block"
+    signal = "corp.ofac" 
+  }
+}
